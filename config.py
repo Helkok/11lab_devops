b608e967fa05ea24cebd079dde748b271e2e76e2
Alexander <fortuna12_12@mail.ru>
2024-12-21 17:59:41 +0700

    добавил поле POSTGRES_URL в класс настроек

--- config.py
+++ config.py
@@ -1,16 +1,29 @@
+from typing import Optional
+
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_PORT: int
+    POSTGRES_PORT: int
     POSTGRES_PASSWORD: str
     POSTGRES_USER: str
     POSTGRES_DB: str
     POSTGRES_HOST: str
-    POSTGRES_HOSTNAME: str
+
+    POSTGRES_URL: Optional[str] = None
+
+    JWT_SECRET_KEY: str
+    JWT_ALGORITM: str
+
+    ACCESS_TOKEN_EXPIRES_MIN: int
 
     class Config:
         env_file = '.env'
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.POSTGRES_URL = f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
 
 
 settings = Settings()
