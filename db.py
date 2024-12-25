e14d403b01b9345e24440ee81082c03b383bcbd2
Alexander <fortuna12_12@mail.ru>
2024-12-25 22:08:20 +0700

    добавил в базовый класс таблиц поля created_at и update_at

--- db.py
+++ db.py
@@ -1,9 +1,16 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker, AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase, Mapped
 from config import settings
+from base import created_at, updated_at
+
 
 
 engine = create_async_engine(settings.POSTGRES_URL, future=True, echo=True)
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False, )
 
-Base = declarative_base()
+
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True
+
+    created_at: Mapped[created_at]
+    updated_at: Mapped[updated_at]
