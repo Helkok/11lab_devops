6bcb14d71d86f803dff56a09e3abe55b8c01d9bd
Alexander <fortuna12_12@mail.ru>
2024-12-25 22:08:56 +0700

    + роутеры auth и user с авторизацией

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.security import HTTPBearer
+from starlette.requests import Request
 
 from middleware.middleware import DBSessionMiddleware
+from user.routers import router as user_router
+from auth.routers import router as auth_router
+from utils import get_current_user
 
 app = FastAPI()
 app.add_middleware(DBSessionMiddleware)
+
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+PROTECTED = [Depends(get_current_user)]
+app.include_router(auth_router, prefix="/auth", tags=["auth"])
+app.include_router(user_router,dependencies=PROTECTED, prefix="/user", tags=["user"])
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+
